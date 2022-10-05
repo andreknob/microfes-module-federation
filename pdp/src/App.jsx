9baef6384298f121ac2720132b611d0433c95ef5
André Knob <dextra.aafonso@creditas.com>
2022-10-05 01:09:27 -0300

    feat: adding lazy loading for header on pdp

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import Header from "home/Header";
+const Header = React.lazy(() => import("home/Header"));
 import Footer from "home/Footer";
 
 const App = () => (
   <div className="mx-auto max-w-6xl">
-    <Header />
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <Header />
+    </React.Suspense>
     <div className="my-10">PDP Content</div>
     <Footer />
   </div>
