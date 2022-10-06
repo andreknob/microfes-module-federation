9e6018c8fb48eca5ceb2b29cff3569a927a284a4
André Knob <dextra.aafonso@creditas.com>
2022-10-06 00:47:29 -0300

    feat: adding routing to pdp

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./index.scss";
 
-const Header = React.lazy(() => import("home/Header"));
+import Header from "home/Header";
 import Footer from "home/Footer";
+import PDPContent from "./PDPContent";
 
 const App = () => (
-  <div className="mx-auto max-w-6xl">
-    <React.Suspense fallback={<div>Loading...</div>}>
+  <Router>
+    <div className="mx-auto max-w-6xl">
       <Header />
-    </React.Suspense>
-    <div className="my-10">PDP Content</div>
-    <Footer />
-  </div>
+      <div className="my-10">
+        <Routes>
+          <Route path="/product/:id" element={<PDPContent />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </Router>
 );
 
 ReactDOM.render(<App />, document.getElementById("app"));
