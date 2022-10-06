cf17dba79ff5c7835aaeaafacdba1c521bc88b61
André Knob <dextra.aafonso@creditas.com>
2022-10-05 22:18:09 -0300

    chore: adding products list on server

--- server/src/modules/products/products.controller.ts
+++ server/src/modules/products/products.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import products, { Product } from '../../products';
 
 @Controller('products')
 export class ProductsController {
   constructor() {}
 
   @Get()
-  async index(): Promise<boolean> {
-    return true;
+  async index(): Promise<Product[]> {
+    return products;
   }
 }
