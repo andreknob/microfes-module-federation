3bb0dbf3bc6344a245e287509da3491a67c7939f
André Knob <dextra.aafonso@creditas.com>
2022-10-09 17:30:04 -0300

    feat: adding MiniCart component to Header

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "cart/Login";
+import MiniCart from "cart/MiniCart";
 
 export default () => (
   <div className="p-5 bg-blue-500 text-white text-3xl">
@@ -7,6 +8,7 @@ export default () => (
       <div className="flex-grow">Fidget Spinner World</div>
       <div className="flex-end relative">
         <Login />
+        <MiniCart />
       </div>
     </div>
   </div>
