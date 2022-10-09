218328a3ceae8cc2c885e0a021ace888f26fd545
André Knob <dextra.aafonso@creditas.com>
2022-10-09 17:19:09 -0300

    feat: adding Login component to Header and other apps

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Login from "cart/Login";
 
 export default () => (
   <div className="p-5 bg-blue-500 text-white text-3xl">
-    Fidget Spinner World
+    <div className="flex">
+      <div className="flex-grow">Fidget Spinner World</div>
+      <div className="flex-end relative">
+        <Login />
+      </div>
+    </div>
   </div>
 );
--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 //import { Link } from "react-router-dom";
 import { getProducts, currency } from "./products";
+import { useLoggedIn } from "cart/cart";
 
 export default function HomeContent() {
+  const isLoggedIn = useLoggedIn();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -25,6 +27,17 @@ export default function HomeContent() {
             <div className="flex-end">{currency.format(product.price)}</div>
           </div>
           <div className="text-sm mt-4">{product.description}</div>
+          {isLoggedIn && (
+            <div className="text-right mt-2">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+                onClick={() => {}} //addToCart(product.id)}
+                id={`addtocart_${product.id}`}
+              >
+                Add to Cart
+              </button>
+            </div>
+          )}
         </div>
       ))}
     </div>
