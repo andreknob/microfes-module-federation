dee093745105facade793479d46f41e1f13258c8
André Knob <dextra.aafonso@creditas.com>
2022-10-10 20:03:33 -0300

    feat: adding MainLayout component

---
+++ home/src/MainLayout.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Header from "home/Header";
+import Footer from "home/Footer";
+import CartContent from "cart/CartContent";
+import HomeContent from "home/HomeContent";
+import PDPContent from "pdp/PDPContent";
+
+const MainLayout = () => (
+  <Router>
+    <div className="mx-auto max-w-6xl">
+      <Header />
+      <div className="my-10">
+        <Routes>
+          <Route exact path="/" element={<HomeContent />} />
+          <Route path="/product/:id" element={<PDPContent />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </Router>
+);
+
+export default MainLayout;
